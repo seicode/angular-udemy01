2e350d2a3307f3f0f565aa43d9e3ae0b3a46666b
seicode <nv7423@gmail.com>
2020-05-02 13:06:33 +0900

    Updated app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { TypingComponent } from './typing/typing.component';
+import { PagesComponent } from './pages/pages.component';
+import { PwGeneratorComponent } from './pw-generator/pw-generator.component';
+
+const appRoutes: Routes = [
+  { path: 'pw-generator', component: PwGeneratorComponent },
+  { path: 'typing', component: TypingComponent },
+  { path: 'pages', component: PagesComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypingComponent,
+    PagesComponent,
+    PwGeneratorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
