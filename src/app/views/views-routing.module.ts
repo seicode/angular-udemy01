c9ed2cd31e839c6072d22fb23e7c8b0bdf87f615
seicode <nv7423@gmail.com>
2020-05-05 00:24:40 +0900

    added the routing to views components with lazy loading

--- src/app/views/views-routing.module.ts
+++ src/app/views/views-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewsHomeComponent } from './views-home/views-home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'views', component: ViewsHomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
