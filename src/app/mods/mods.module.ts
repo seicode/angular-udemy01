9789d519fcca2a4a8404861e5be8cfc436adc174
seicode <nv7423@gmail.com>
2020-05-05 00:38:00 +0900

    applied lazy loading to mods components

--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module'
 import { ModsRoutingModule } from './mods-routing.module';
+import { ModsHomeComponent } from './mods-home/mods-home.component';
+import { ModalComponent } from './modal/modal.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ModsHomeComponent, ModalComponent],
   imports: [
     CommonModule,
-    ModsRoutingModule
+    ModsRoutingModule,
+    SharedModule
   ]
 })
 export class ModsModule { }
